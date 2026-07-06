464a53ebab6596037aa8718f24475b8d2eddf775
DI0IK <76131420+DI0IK@users.noreply.github.com>
2026-07-06 08:28:23 +0000

    Fix version numbering for obtainium update detection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -173,7 +173,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
